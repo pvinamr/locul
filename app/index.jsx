67b423d517212bba09f668ba7e1f8c75fb16b646
Vinamr Pemmaraju <pvinamr@gmail.com>
2025-07-22 11:12:58 -0400

    Adding styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 const Home  = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style = {styles.container}>
+      <Text style = {styles.title}>Home</Text>
+      <Text style = {{marginTop: 10, marginBottom: 30}}>Locul App</Text>
+
+      <View style = {styles.card}>
+        <Text> This is a card</Text>
+        
+      </View>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    //defining css properties
+    flex: 1, //takes up screen from top to bottom
+    alignItems: 'center',
+    justifyContent: 'center' //y axis alignment
+  },
+
+  title:{
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+
+  card:{
+    backgroundColor: '#eee',
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: '4px 4px rgba(0,0,0,0.1)'
+  }
+})
