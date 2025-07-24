0d048c826f5202727abb3f96247eb7265ba808c3
Vinamr Pemmaraju <pvinamr@gmail.com>
2025-07-24 13:37:30 -0400

    revert 1

--- app/index.jsx
+++ app/index.jsx
@@ -1,50 +1,14 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import Logo from '../assets/favicon.png'
-import { Link } from 'expo-router'
 
-const Home  = () => {
+const index = () => {
   return (
-   <View style = {styles.container}>
-      <Image source={Logo} style={styles.img} />
-      <Text style = {styles.title}>Home</Text>
-      <Text style = {{marginTop: 10, marginBottom: 30}}>Locul App</Text>
-      <Link href = "/login" style = {styles.link}>Login Page</Link>
-      <Link href = "/register" style = {styles.link}>Register Page</Link>
-      <Link href = "/profile" style = {styles.link}>Profile Page</Link>
+    <View>
+      <Text>index</Text>
     </View>
   )
 }
 
-export default Home
+export default index
 
-const styles = StyleSheet.create({
-  container: {
-    //defining css properties
-    flex: 1, //takes up screen from top to bottom
-    alignItems: 'center',
-    justifyContent: 'center' //y axis alignment
-  },
-
-  title:{
-    fontWeight: 'bold',
-    fontSize: 18
-  },
-
-/*  card:{
-    backgroundColor: '#eee',
-    padding: 20,
-    borderRadius: 5,
-    boxShadow: '4px 4px rgba(0,0,0,0.1)'
-  },
-*/
-  img: {
-    marginVertical: 20,
-  },
-
-  link:{
-    marginVertical: 10,
-    borderBottomWidth: 1
-  }
-
-})
+const styles = StyleSheet.create({})
